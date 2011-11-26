3d4a91692591fd911072d61fb06c9b778072d087
doctor_butch <doctor_butch@5a087cdc-7f68-4932-a9f5-0893246b91dc>
2011-11-26 07:10:12 +0000

    Rewrote update to record max child update for every node and started reading code

--- branches/html5/world.php
+++ branches/html5/world.php
@@ -1,100 +1,128 @@
 <?php
+
 /**
  * AJAX service to save and manipulate the game world
  *
- * The world is essentially a JSON associative array.
- *
- * In addition, each element of the world has a last modification stamp.  The stamp
- * is a monotonically increasing number (so the stamp for newer updates are greater
- * than older updates), with a starting stamp of 0.
+ * The world is a single world node.  A world node has:
+ *  - A value: an int, string, or array of world nodes
+ *  - An assignment stamp: when it was last directly assigned to (or new)
+ *  - A max child assignment stamp: when it or its children were updated
  */
-
 $filename = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "bgaworld.json";
-$action = isset($_REQUEST["action"])?$_REQUEST["action"]:"read";
+$action = isset($_REQUEST["action"]) ? $_REQUEST["action"] : "read";
 
-if ($action == "read"){
-	$lastmodify = isset($_REQUEST["lastmodify"])?$_REQUEST["lastmodify"]:"-1";
+if ($action == "read") {
+	$last_modify = isset($_REQUEST["last_modify"]) ? $_REQUEST["last_modify"] : "-1";
 	$file = @fopen($filename, "r");
 	if ($file) {
-		flock($file,LOCK_SH);
+		flock($file, LOCK_SH);
 	}
-	$data = read_world_file($filename);
-
+	$world = read_world_file($filename);
 	if ($file) {
-		flock($file,LOCK_UN);
+		flock($file, LOCK_UN);
 		fclose($file);
 	}
+	$update = &get_world_update($world, $last_modify);
 	echo "\n\n";
-	print_r($data);
+	echo json_encode($update);
+	echo "<P>";
+	echo json_encode($world);
 	exit();
-} else if ($action == "update"){
-	if (!isset($_REQUEST["update"])){
+} else if ($action == "update") {
+	if (!isset($_REQUEST["update"])) {
 		exit_json_error("Must set update value");
 	}
-	$update = json_decode(stripslashes($_REQUEST["update"]),true);
+	$update = json_decode(stripslashes($_REQUEST["update"]), true);
+	if (json_last_error() != JSON_ERROR_NONE) {
+		exit_json_error("Error parsing update value");
+	}
 	// Lock and load the world :)
 	$file = @fopen($filename, "c+");
-	if (!$file){
+	if (!$file) {
 		exit_json_error("Unable to open $filename");
 	}
-	flock($file,LOCK_EX);
-	$data = read_world_file($filename);
-	$data["maxlastmodify"] += 1;
-	world_node_update($data["world"],$data["lastmodify"],$update,$data["maxlastmodify"]);
-	ftruncate($file,0);
-	fwrite($file,json_encode($data));
-	flock($file,LOCK_UN);
+	flock($file, LOCK_EX);
+	$world = read_world_file($filename);
+	world_node_update($world, $update, $world["max_assigned"] + 1);
+	ftruncate($file, 0);
+	fwrite($file, json_encode($world));
+	flock($file, LOCK_UN);
 	echo "\n\n Update = ";
 	print_r($update);
 	echo "<P>\n\n";
-	print_r($data);
+	print_r($world);
 	exit();
 } else {
 	exit_json_error("Unknown action");
 }
 
-function exit_json_error($error_text){
+function exit_json_error($error_text) {
 	echo "\n\n";
 	echo json_encode(array("error" => $error_text));
 	exit();
 }
 
-function read_world_file($filename){
-	$file_data = @file_get_contents($filename);
-	if (!empty($file_data)){
-		$data = json_decode($file_data,true);
-	} else {
-		$data = array("world" => null,
-			"lastmodify" => array("__ts" => 0),
-			"maxlastmodify" => 0);
-	}
-	return $data;
+function assign_world_node(&$world_node, $value, $assign_stamp) {
+	$world_node["value"] = $value;
+	$world_node["assigned"] = $assign_stamp;
+	$world_node["max_assigned"] = $assign_stamp;
 }
 
-function world_node_update(&$world_node,&$lastmodify,&$update,$ts){
-	if (!is_array($world_node) || !is_array($update)){
-		$world_node = $update;
-		$lastmodify = array("__ts" => $ts);
+function read_world_file($filename) {
+	$file_data = @file_get_contents($filename);
+	if (!empty($file_data)) {
+		$world = json_decode($file_data, true);
 	} else {
-		if (!is_array($lastmodify)){
-			$lastmodify = array();
+		$world = array();
+		assign_world_node($world, null, 0);
+	}
+	return $world;
+}
+
+function world_node_update(&$world_node, &$update, $ts) {
+	if (!is_array($update)) {
+		assign_world_node($world_node, $update, $ts);
+	} else {
+		// If we don't have children yet or
+		// received the __new directive, create a new array for the value
+		if (!is_array($world_node["value"]) ||
+				array_key_exists("__new", $update)) {
+			assign_world_node($world_node, array(), $ts);
 		}
-		// If we have the __new directive, clear out the node
-		if (array_key_exists("__new",$update)){
-			$world_node = array();
-			$lastmodify = array("__ts" => $ts);
-		}
-		foreach($update as $key => &$value){
-			if ($key == "__new") continue; // Don't copy __new derective
-			if (!array_key_exists($key,$world_node)){
-				$world_node[$key] = $value;
-				$lastmodify[$key] = array("__ts" => $ts);
-			} else {
-				world_node_update($world_node[$key],$lastmodify[$key],$value,$ts);
+		// One of our children is being updated so update our max_assigned
+		$world_node["max_assigned"] = $ts;
+		$children = &$world_node["value"];
+		foreach ($update as $key => &$value) {
+			if ($key == "__new") {
+				continue; // Don't copy __new directive
 			}
+			if (!array_key_exists($key, $children)) {
+				$children[$key] = array();
+				assign_world_node($children[$key],null,$ts);
+			}
+			world_node_update($children[$key], $value, $ts);
 		}
 		unset($value); // Make sure following lines don't change the last element in the array
 	}
+}
+
+function &get_world_update(&$world_node, $ts) {
+	if (!is_array($world_node["value"])){
+		$update = $world_node["value"];
+	} else {
+		$update = array();
+		// If newly assigned, send the __new directive
+		if ($world_node["assigned"] >= $ts) {
+			$update["__new"] = 1;
+		}
+		$children = $world_node["value"];
+		foreach ($children as $key => $child){
+			if ($child["max_assigned"] >= $ts) {
+				$update[$key] = &get_world_update($child,$ts);
+			}
+		}
+	}
+	return ($update);
 }
 
 ?>
