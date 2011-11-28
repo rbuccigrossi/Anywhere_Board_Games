2df4800bfbab0b826376f8ebae67c991bf4bed39
doctor_butch <doctor_butch@5a087cdc-7f68-4932-a9f5-0893246b91dc>
2011-11-28 15:50:09 +0000

    Got dialog and popup working together

--- branches/html5/index.php
+++ branches/html5/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>GBA</title>
   <link href="css/smoothness/jquery-ui.css" rel="stylesheet" type="text/css"/>
   <script src="js/jquery.min.js" type="text/javascript"></script>
   <script src="js/jquery-ui.min.js" type="text/javascript"></script>
@@ -159,6 +160,7 @@ $(document).ready(function() {
 /* CONTEXT MENU FUNCTIONALITY */
 $.fn.setUpContextMenu = function() {
 	$(this).dialog({
+		dialogClass: 'popup bga_small_text_dialog',
 		autoOpen: false,
 		modal: true,
 		resizable: false,
@@ -176,12 +178,16 @@ $.fn.openContextMenu = function(jsEvent) {
 	menu.css('padding', 0);
 	menu.dialog('option','position',[jsEvent.clientX, jsEvent.clientY]);
 	menu.bind('dialogopen', function(event, ui) {
-		$('.ui-dialog-titlebar').hide();
+		$('.popup .ui-dialog-titlebar').hide();
 		$('.ui-widget-overlay').unbind('click');
 		$('.ui-widget-overlay').css('opacity',0);
-		$('.ui-widget-overlay').click(function() {
+		$('.ui-widget-overlay').bind('mousedown',function() {
 			menu.dialog('close');
+			return true;
 		});
+	});
+	menu.bind('focus', function(event, ui){   // Remove first item being hovered
+		menu.find('a').removeClass('ui-state-focus ui-state-hover');
 	});
 	menu.dialog('open');
 
@@ -196,16 +202,51 @@ $(document).ready(function() {
 		return false;
 	});
 });
+
+$(document).ready(function() {
+	$( "#create_piece_dialog" ).dialog({
+		dialogClass: 'bga_dialog bga_small_text_dialog',
+		autoOpen: false,
+		height: 300,
+		width: 350,
+		modal: true,
+		buttons: {
+			"Create a piece": function() {
+				$( this ).dialog( "close" );
+			},
+			Cancel: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+	});
+});
+
+
 </script>
+<style>
+	.bga_small_text_dialog { font-size: 67.5%; }
+	.bga_dialog label, .bga_dialog input { display:block; }
+	.bga_dialog input.text { margin-bottom:12px; width:95%; padding: .4em; }
+	.bga_dialog fieldset { padding:0; border:0; margin-top:25px; }
+
+</style>
 </head>
 <body style="background-color:#A0A0A0;">
 <!-- scrolling="no" -->
 <div id="info"></div>
 <div id="background_context_menu" class="ContextMenu">
-	<a href="javascript:void(0);" onClick="$('#background_context_menu').dialog('close'); alert('new piece');" id="new_piece">New Piece</a>
+	<a href="javascript:void(0);" onClick="$('#background_context_menu').dialog('close'); $( '#create_piece_dialog' ).dialog('open');" id="new_piece">New Piece</a>
 	<a href="javascript:void(0)" onClick="$('#background_context_menu').dialog('close');" id="option2">Option 2</a>
 	<a href="javascript:void(0)" id="option3">Option 3</a>
 	<a href="javascript:void(0)" id="option4">Option 4</a>
+</div>
+<div id="create_piece_dialog" title="Create a New Piece">
+	<form>
+	<fieldset>
+		<label for="url">Image URL</label>
+		<input type="text" name="url" id="url" class="text ui-widget-content ui-corner-all" />
+	</fieldset>
+	</form>
 </div>
 <span class="piece" style="position: absolute; left: 50px; top: 50px;">
   <img class="piece_face" src="piece.png">
