58452db0c283a766b31c7f474405c9633a644d3a
doctor_butch <doctor_butch@5a087cdc-7f68-4932-a9f5-0893246b91dc>
2012-01-02 06:37:28 +0000

    [#3468279] Fixing bug with chrome scrollbar not being clicable

--- client/util.js
+++ client/util.js
@@ -1,3 +1,21 @@
+
+// Detection used for Chrome-specific bug fixes
+var util_is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+
+// Bug fix for chrome to allow scroll bars to work on mouse down events:
+function util_is_in_chrome_scrollbar(){
+	if (util_is_chrome){ // Don't allow clicks near scroll bars
+		console.log("" + event.clientX + " " + $(document).width() + " " + $(window).width());
+		console.log("" + event.clientY + " " + $(document).height() + " " + $(window).height());
+		if ((event.clientX > ($(window).width()-17)) ||
+			(event.clientY > ($(window).height()-17))){
+			return (true);
+		}
+	}
+	return (false);
+}
+
+
 /*
  * util_page_to_client_coord - converts page to client coordinates based upon 
  * the current scrollbar location
