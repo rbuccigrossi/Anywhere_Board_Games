35cab3855459e66ede1d622ae2005567ee93b012
doctor_butch <doctor_butch@5a087cdc-7f68-4932-a9f5-0893246b91dc>
2012-07-04 06:39:10 +0000

    First attempt at implementing a Google Hangout application

---
+++ trunk/hangout/popup_menu.js
@@ -0,0 +1,70 @@
+/*
+ * Creates a simple pop-up menu.
+ *
+ * @param menu_items_config Array of (label, callback, args) to create the menu items
+ * @param parent DOM object into which the pop-up menu is placed
+ * @param position Client position (top, left) pair for the placement of the menu
+ * @param cancel_callback A callback if the menu is closed by clicking the overlay
+ * @return menu The jQuery dialog object
+ */
+
+function create_popup_menu(menu_items_config, parent, position, cancel_callback){
+	var i, menu, menu_item;
+	// Create and add the menu items
+	menu = $('<div style="display:none;"></div>');
+	// Add to the parent
+	parent.append(menu);
+	// Given a menu config, this creates a hander to call the callback and close the dialog
+	function popup_callback_maker(config){
+		return function(event) { 
+			menu.dialog('close');
+			menu.remove();
+			return config.callback(event,config.args);
+		};		
+	}
+	// Now create the menu items with the appropriate callback
+	for( i in menu_items_config){
+		menu_item = $('<a href="javascript: void(0);">' +
+			menu_items_config[i].label + '</a>');
+		menu_item.num = i;
+		menu_item.bind('click',popup_callback_maker(menu_items_config[i]));
+		menu.append(menu_item);
+	}
+	// Style the menu
+	menu.css('padding', 0);
+	// Turn links unto jquery ui buttons
+	menu.find('a').css('display','block').button();
+	// Set up the dialog box
+	menu.dialog({
+		dialogClass: 'popup bga_small_text_dialog',
+		autoOpen: false,
+		modal: true,
+		resizable: false,
+		width: '150',
+		height: 'auto',
+		minHeight: 'auto',
+		minWidth: '150'
+	});
+	menu.dialog('option','position',[position.left, position.top]);
+	// When the dialog opens, set the overlay to close the dialog if you click elsewhere
+	menu.bind('dialogopen', function(event, ui) {
+		$('.popup .ui-dialog-titlebar').hide();
+		$('.ui-widget-overlay').unbind('click');
+		$('.ui-widget-overlay').css('opacity',0);
+//		$('.ui-widget-overlay').bind('mousedown touchstart',function(event) {
+		$('.ui-widget-overlay').bind('click',function(event) {
+			menu.dialog('close');
+			menu.remove();
+			if (cancel_callback){
+				cancel_callback(event);
+			}
+			event.preventDefault();
+			return false;
+		});
+		// Unset the hover that the jquery dialog places on the first item
+		menu.find('a').removeClass('ui-state-focus ui-state-hover');
+	});
+	// Now open the dialog
+	menu.dialog('open');
+	return(menu);
+}
---
+++ trunk/hangout/util.js
@@ -0,0 +1,157 @@
+
+// Detection used for Chrome-specific bug fixes
+var util_is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+
+// Bug fix for chrome to allow scroll bars to work on mouse down events:
+function util_is_in_chrome_scrollbar(){
+	if (util_is_chrome){ // Don't allow clicks near scroll bars
+		if ((event.clientX > ($(window).width()-17)) ||
+			(event.clientY > ($(window).height()-17))){
+			return (true);
+		}
+	}
+	return (false);
+}
+
+
+/*
+ * util_page_to_client_coord - converts page to client coordinates based upon 
+ * the current scrollbar location
+ * 
+ * @param page The (left, top) page coordinates
+ * @return client The (left, top) client coordinates
+ */
+function util_page_to_client_coord(page){
+	var left = page.left;
+	var top = page.top;
+    if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
+      left -= document.body.scrollLeft;
+      top -= document.body.scrollTop;
+    } else if( document.documentElement && ( document.documentElement.scrollLeft || document.documentElement.scrollTop ) ) {
+      left -= document.documentElement.scrollLeft;
+      top -= document.documentElement.scrollTop;
+	}
+	return ({left: left, top: top});
+}
+
+/*
+ * util_is_touch_event - Helper function to determine if an event is a touch event
+ * 
+ * @param event The mouse or touch event
+ */
+function util_is_touch_event(event){
+	return ((typeof event.touches != 'undefined') || (typeof event.changedTouches != 'undefined'));
+}
+
+/*
+ * util_ignore_click_from_touch - An implementation of the Google "clickbuster"
+ * to ignore clicks that result from touch events we have already handled
+ * see http://code.google.com/mobile/articles/fast_buttons.html
+ * 
+ * Here we try a simpler implementation that ignores clicks for the next 0.5 seconds.
+ */
+function util_ignore_click_from_touch(){
+	var ignore_callback = function(event){
+		event.stopPropagation();
+		event.preventDefault();
+	};
+	var remove_callback = function(){
+		document.removeEventListener('click',ignore_callback,true);
+	}
+	document.addEventListener('click',ignore_callback,true);
+	setTimeout(remove_callback, 1000);
+}
+
+/*
+ * util_get_event_coordinates - Helper function to get the event coordinates for either a 
+ * mouse or touch event
+ * 
+ * @param event The mouse or touch event
+ * @return coord (x, y) coordinates for the event
+ */
+function util_get_event_coordinates(event){
+	var coord;
+	if (util_is_touch_event(event)){
+		// If this is a touch event, use the first touch
+		// TODO: LOW - Allow multiple touches and use the closest touch to the object (targetTouches)
+		if (event.touches.length > 0){
+			coord = {
+				x: event.touches[0].pageX, 
+				y: event.touches[0].pageY
+			};
+		} else {
+			coord = {
+				x: event.changedTouches[0].pageX, 
+				y: event.changedTouches[0].pageY
+			};			
+		}
+	} else {
+		coord = {
+			x: event.pageX, 
+			y: event.pageY
+		};
+	}
+	return (coord);
+}
+
+/*
+ * util_clone - Does a shallow clone of an object or array
+ * 
+ * @param orig Original object or array
+ */
+function util_clone(orig){
+	if (orig instanceof Array){
+		return orig.slice(0);
+	} else {
+		return ($.extend({},orig));
+	}
+}
+/*
+ * util_create_ui_overlay - Generates a ui-widget-overlay, sets the window resize
+ * callback to manipulate it, and returns the DOM object
+ *
+ * @param click_callback Callback(event) for click event
+ * @return ui_overlay DOM object
+ */
+function util_create_ui_overlay(click_callback){
+	var js_overlay = $('<div class="ui-widget-overlay"></div>').css('opacity',0).appendTo('body');
+	js_overlay.css('position','absolute');
+	js_overlay.css('z-index',1002);
+	js_overlay.width($(document).width());
+	js_overlay.height($(document).height());
+	var set_overlay_dimension = function (){
+		js_overlay.width($(document).width());
+		js_overlay.height($(document).height());
+	}
+	$(window).bind("resize",set_overlay_dimension);
+	js_overlay.bind("click",function(event){
+		$(window).unbind("resize",set_overlay_dimension);
+		js_overlay.remove();
+		if (click_callback){
+			return (click_callback(event));
+		}
+		else {
+			return true;
+		}
+	});
+	return (js_overlay.get(0))
+}
+
+function util_ignore_event(event){
+	event.preventDefault();
+	return(false);
+}
+
+
+function util_get_browser_width(){
+  if( typeof( window.innerWidth ) == 'number' ) {
+    //Non-IE
+    return window.innerWidth;
+  } else if( document.documentElement && document.documentElement.clientWidth ) {
+    //IE 6+ in 'standards compliant mode'
+    return document.documentElement.clientWidth;
+  } else {
+    //IE 4 compatible
+    return document.body.clientWidth;
+  }
+}
